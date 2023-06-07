13ba10054a7dad1922d19cd3974bf401d2021b1c
Arkadiusz Florek <arekflo123@gmail.com>
2023-06-07 06:34:43 +0200

    #

---
+++ Projekt/Dane1/skrpyt_danepy.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+
+
+tabela = pd.read_csv("https://www.kaggle.com/datasets/ajaypalsinghlo/world-happiness-report-2021.csv")
+
+print(tabela)
